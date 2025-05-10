13077a2d37a3f75fa79e44e22484dc1f7c4cfd5a
garin <glmccullick@gmail.com>
2025-05-10 00:56:59 -0500

    updtae

--- fbg/src/components/ApplyLoginOverlay.js
+++ fbg/src/components/ApplyLoginOverlay.js
@@ -3,13 +3,20 @@ import "../App.css";
 import { FaDiscord } from "react-icons/fa";
 
 const ApplyLoginOverlay = () => {
+  const handleDiscordLogin = () => {
+    window.location.href = "http://localhost:5000/oauth/login";
+  };
   return (
     <div className="apply-form-login-overlay">
       <h2 className="overlay-title">Login Required</h2>
       <p className="overlay-message">
         Please log in to continue with your application.
       </p>
-      <button className="discord-login-button" type="button">
+      <button
+        className="discord-login-button"
+        type="button"
+        onClick={handleDiscordLogin}
+      >
         <span className="discord-icon">
           <FaDiscord />
         </span>
